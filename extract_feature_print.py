37f21866272a033221fbfaf614b9915c0ce6c3a9
Optic the Wolf <65792938+Optic-the-Wolf@users.noreply.github.com>
2023-09-16 00:17:08 -0400

    Update extract_feature_print.py

--- extract_feature_print.py
+++ extract_feature_print.py
@@ -66,7 +66,7 @@ printt("load model(s) from {}".format(model_path))
 # if hubert model is exist
 if os.access(model_path, os.F_OK) == False:
     printt(
-        "Error: Extracting is shut down because %s does not exist, you may download it from https://huggingface.co/lj1995/VoiceConversionWebUI/tree/main"
+        "Error: Extracting is shut down because %s does not exist"
         % model_path
     )
     exit(0)
